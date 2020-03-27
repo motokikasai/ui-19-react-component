f756a923fa15aafb8b235cbc308735116c66a072
motokikasai <kmomoky@hotmail.com>
2020-03-27 23:28:34 +0100

    Create a new contact object with icons in order to loop through data & respective icons in each iteration

--- src/components/social-link-item/index.js
+++ src/components/social-link-item/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-import phone from "../../assets/phone.svg";
-// import email from "../../assets/mail.svg";
-// import url from "../../assets/url.svg";
-
 function SocialLinkItem(props) {
   return (
     <section className="social-item link-phone">
       <a href="google.com" target="_blank">
         <span>
-          <img src={phone} alt="" className="icon" />
+          <img
+            src={props.contactInfo.icon}
+            alt={props.contactInfo.icon}
+            className="icon"
+          />
         </span>
-        {props.datum}
+        {props.contactInfo.datum}
       </a>
     </section>
   );
--- src/components/social-links-list/index.js
+++ src/components/social-links-list/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import SocialLinkItem from "../social-link-item";
 
-// import phone from "../../assets/phone.svg";
-// import email from "../../assets/mail.svg";
-// import url from "../../assets/url.svg";
+import phone from "../../assets/phone.svg";
+import email from "../../assets/mail.svg";
+import url from "../../assets/url.svg";
 
 function SocialSection(props) {
-  // const icons = [phone, email, url];
+  // Create a new OBJECT with icons 'woven into' the original contact object
+  const contactWithIcon = {
+    phone: {
+      datum: props.sns.phone,
+      icon: phone
+    },
+    email: {
+      datum: props.sns.email,
+      icon: email
+    },
+    url: {
+      datum: props.sns.url,
+      icon: url
+    }
+  };
 
   return (
     <div className="social">
       <div className="contact-info">
-        {Object.values(props.sns).map((datum, key) => {
-          return <SocialLinkItem key={key} datum={datum} />;
+        {Object.values(contactWithIcon).map((contactInfo, key) => {
+          return <SocialLinkItem key={key} contactInfo={contactInfo} />;
         })}
       </div>
     </div>
