f4e14af89480c451b614056e81ddf05da78d78b0
motokikasai <kmomoky@hotmail.com>
2020-03-27 13:36:07 +0100

    Implement all the data from data.json EXCEPT for social icons

--- src/components/social-links-list/index.js
+++ src/components/social-links-list/index.js
@@ -1,38 +1,19 @@
 import React from "react";
 import SocialLinkItem from "../social-link-item";
 
+// import phone from "../../assets/phone.svg";
+// import email from "../../assets/mail.svg";
+// import url from "../../assets/url.svg";
+
 function SocialSection(props) {
+  // const icons = [phone, email, url];
+
   return (
     <div className="social">
       <div className="contact-info">
         {Object.values(props.sns).map((datum, key) => {
           return <SocialLinkItem key={key} datum={datum} />;
         })}
-
-        {/* <section className="social-item link-phone">
-          <a href="google.com" target="_blank">
-            <span>
-              <img src={phone} alt="" className="icon" />
-            </span>
-            {props.sns.contact.phone}
-          </a>
-        </section>
-        <section className="social-item link-email">
-          <a href="google.com" target="_blank">
-            <span>
-              <img src={email} alt="" className="icon" />
-            </span>
-            {props.sns.contact.email}
-          </a>
-        </section>
-        <section className="social-item link-url">
-          <a href="google.com" target="_blank">
-            <span>
-              <img src={url} alt="" className="icon" />
-            </span>
-            {props.sns.contact.url}
-          </a>
-        </section> */}
       </div>
     </div>
   );
